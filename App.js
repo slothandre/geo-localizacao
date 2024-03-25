88cce692f518a56999595fa5bc13c953ababe39f
André Marques <andre.m.d.abreu@gmail.com>
2024-03-25 09:26:23 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import { Image, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  /* Coordenadas para o MapView */
   const regiaoInicialMapa = {
+    /* // Brasil
     latitude: -10,
-    longitude: -55,
+    longitude: -55, */
+
+    // São Paulo
+    latitude: -23.533773,
+    longitude: -46.65529,
 
     /* Definição do zoom do mapa.
     Quanto menor, mais próximo o mapa.
     Quanto maior, mais longe o mapa fica */
     latitudeDelta: 40,
     longitudeDelta: 40,
+  };
+
+  /* Coordenadas para o Marker que será
+  aplicado ao MapView */
+  const localizacao = {
+    latitude: -33.867886,
+    longitude: -63.987,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
   };
   return (
     <>
@@ -21,9 +36,13 @@ export default function App() {
           mapType="standard"
           style={styles.mapa}
           initialRegion={regiaoInicialMapa}
-          maxZoomLevel={15}
-          minZoomLevel={5}
-        />
+          // maxZoomLevel={15}
+          // minZoomLevel={5}
+        >
+          <Marker coordinate={localizacao}>
+            <Image source={require(`./assets/ghost.png`)} />
+          </Marker>
+        </MapView>
       </View>
     </>
   );
