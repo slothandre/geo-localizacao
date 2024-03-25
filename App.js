21a8bec7c5e1a54b6c1c9b3beb45026f070c2218
André Marques <andre.m.d.abreu@gmail.com>
2024-03-25 09:03:12 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,11 +3,27 @@ import { StyleSheet, Text, View } from "react-native";
 import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicialMapa = {
+    latitude: -10,
+    longitude: -55,
+
+    /* Definição do zoom do mapa.
+    Quanto menor, mais próximo o mapa.
+    Quanto maior, mais longe o mapa fica */
+    latitudeDelta: 40,
+    longitudeDelta: 40,
+  };
   return (
     <>
       <StatusBar style="auto" />
       <View style={styles.container}>
-        <MapView style={styles.mapa} />
+        <MapView
+          mapType="standard"
+          style={styles.mapa}
+          initialRegion={regiaoInicialMapa}
+          maxZoomLevel={15}
+          minZoomLevel={5}
+        />
       </View>
     </>
   );
