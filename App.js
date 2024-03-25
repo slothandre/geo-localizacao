8505673e1828105738c3d991d7140b27442f8ba2
André Marques <andre.m.d.abreu@gmail.com>
2024-03-25 10:48:44 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,28 +5,38 @@ import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 
 export default function App() {
+  /* State para monitorar dados da atualização atual do usuário.
+  Inicialmente, nulo. */
   const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
 
   useEffect(() => {
     async function obterLocalizacao() {
+      /* Acessando o status da requisição de permissão de uso
+      dos recursos de geolocalização. */
       const { status } = await Location.requestForegroundPermissionsAsync();
+
+      /* Se o status NÃO FOR liberado/permitido, então
+      será dado um alerta notificando o usuário. */
       if (status !== "granted") {
         Alert.alert("Ops!", "Você não autorizou o uso de geolocalização");
         return;
       }
 
+      /* Se o status estiver OK, obtemos os dados da localização
+      atual. E atualizamos o state de minhaLocalizacao. */
       let localizacaoAtual = await Location.getCurrentPositionAsync({});
 
       setMinhaLocalizacao();
     }
-  });
+  }, []);
 
-  const [localizacao, setLocalizacao] = useState({
-    latitude: -33.867886,
-    longitude: -63.987,
-    latitudeDelta: 10,
-    longitudeDelta: 10,
-  });
+  console.log(minhaLocalizacao);
+
+  /* Este state tem a finalidade de determinar
+  a posição/localização no MapView junto com o Marker.
+  Inicialmente é nulo pois o usuário ainda não acionou o
+  botão da sua localização. */
+  const [localizacao, setLocalizacao] = useState(null);
 
   /* Coordenadas para o MapView */
   const regiaoInicialMapa = {
@@ -67,9 +77,7 @@ export default function App() {
             style={styles.mapa}
             initialRegion={regiaoInicialMapa}
           >
-            <Marker coordinate={localizacao}>
-              <Image source={require(`./assets/ghost.png`)} />
-            </Marker>
+            {localizacao && <Marker coordinate={localizacao} />}
           </MapView>
         </View>
       </View>
